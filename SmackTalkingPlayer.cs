4ae3cd857b615034064776060af356c49ba9606b
Jacob Babbitt <jacobhbabbitt@gmail.com>
2023-03-09 09:38:43 -0600

    smacktalking and one higher players

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -1,10 +1,21 @@
+using System;
+
 namespace ShootingDice
 {
-    // TODO: Complete this class
-
     // A Player who shouts a taunt every time they roll dice
-    public class SmackTalkingPlayer
+    public class SmackTalkingPlayer : Player
     {
         public string Taunt { get; }
+
+        public SmackTalkingPlayer(string taunt)
+        {
+            Taunt = taunt;
+        }
+
+        public override int Roll()
+        {
+            Console.WriteLine($"{Name} shouts '{Taunt}'");
+            return base.Roll();
+        }
     }
 }
